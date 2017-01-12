3abd8c8785ef9a8e7c063e61eb68c39233508e50
joher <joher@DESKTOP-QFG8DDF>
2017-01-12 12:44:40 +0100

    använde Viewbag för att behålla värdena i formfields

--- loveNet/loveNet/Controllers/RegisterUserController.cs
+++ loveNet/loveNet/Controllers/RegisterUserController.cs
@@ -16,19 +16,26 @@ namespace loveNet.Controllers
         }
 
         
-        //Denna actionmetod har samma namn som den ovan. Attributet "Actionname" gör
+        //Om en actionmetod har samma namn som en annan. Attributet "Actionname" gör
         //att den ändå routar till rätt sida. "HttpPost" gör att denna körs bara när
         //man postar något från viewn.
-        [ActionName("Register")]
+        //[ActionName("Register")]
+        //Parametrarna känns igen från formfields, kallas "modelbinding". Eftersom denna metod har en annan signatur
+        //än den ovan kan man använda samma namn (overloading). Parametern ska matcha formfields "name"
         [HttpPost]
-        public ActionResult RegisterPost()
+        public ActionResult Register(string user_firstname, string user_lastname, int user_age)
         {
             ViewBag.userRegistrated = "Du har lagts till som användare, nu kan du logga in";
             //man kan nå dom olika form values på samma sätt som elementen i en array.
             //istället för myArray[i], skriver man Request.Form[name på den formrad man vill komma åt]
-            string firstName = Request.Form["user_firstname"];
+            string _firstName = user_firstname;
             //man bör kunna skicka med "form" som en parameter till en repositorymetod som
             //lägger till användare i databasen
+
+            //återställ form med viewbag. Egentligen är det en bättre lösning att använda formvalidation
+            ViewBag.VB_firstname = user_firstname;
+            ViewBag.VB_lastname = user_lastname;
+            ViewBag.VB_age = user_age;
             var form = Request.Form;
             return View();
         }
